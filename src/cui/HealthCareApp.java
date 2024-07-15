2cb493c5ef97b218847316850667dc098d34cbcd
Esosa Izekor || <114697608+esosaizekor@users.noreply.github.com>
2024-07-15 18:01:49 +0100

    Add files via upload

---
+++ src/cui/HealthCareApp.java
@@ -0,0 +1,11 @@
+package cui;
+
+import domein.DomeinController;
+
+public class HealthCareApp {
+	
+	public void Start(DomeinController DC)
+	{
+		
+	}
+}
---
+++ src/cui/StartUp.java
@@ -0,0 +1,14 @@
+package cui;
+
+import domein.DomeinController;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new HealthCareApp().Start(new DomeinController());
+		
+		System.out.println("Health Care.");
+	}
+
+}
